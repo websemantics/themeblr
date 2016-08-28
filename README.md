10cfdbb34f06b526e0c3e520e1bb70742450a214
Adnan <adnan@websemantics.ca>
2016-08-28 14:09:54 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# bootstrap-theme-boilerplate
-Bootstrap theme boilerplate build framework
+```
+              :
+        . :   : :
+        ; . : ' : ;
+        : : : . : ;  
+        ; . : ' : ;
+        ____________
+      _/_/_.______/ \
+     | \__________\_/
+     |______   _                         _     _      
+          | \ | |__   ___ _ __ ___   ___| |__ | |_ __
+          | | | '_ \ / _ \ '_ ` _ \ / _ \ '_ \| | '__|
+          | | | | | |  __/ | | | | |  __/ |_) | | |   
+          [_] |_| |_|\___|_| |_| |_|\___|_.__/|_|_|  
+
+          Build Bootstrap Themes the Right Way!
+
+```
+> Themeblr is a powerful themes builder boilerplate for Bootstrap 4. It provides an easy way to extend Bootstrap 4 styles and plugins using Bootstrap grunt builder. Themeblr can also be used as the basis for developing new css frameworks, for example: [Brimer](https://github.com/websemantics/Brimer).
+
+
+## Quick start
+
+First, clone or fork this repository and change the detailes inside `package.json` (name, version etc).
+
+Start building a custom Bootstrap theme or a new css framework by overriding Bootstrap components (`scss` folder) and creating jQuery plugins (`js` folder).
+
+Bootstrap provides a comprehensive list of variables to enable great level of freedom to change the look and feel of the framework.
+
+Copy variables from `node_modules/bootstrap/scss/_variables.scss` to
+the theme variables file at `scss/_variables` to apply the desired changes.
+
+For greater level of control, override the individual components styles by adding new style rules to the components files at, `scss` folder.
+
+
+## Javascript
+
+Because Themeblr uses Bootstrap grunt build system, you can write your own jQuery plugins inside `js` folder. These plugins along-side Bootstrap's will be compiled and distributed with the theme.
+
+
+## build
+
+To build the theme, run Grunt as follows,
+
+```
+grunt
+```
+
+This will generate the theme files at `dist\css` and `dist\js`.
+
+There are other grunt targets that provide support for generating docs, publishing and deployment of the theme.
+
+
+## Creators
+
+**Adnan M.Sagar, PhD**
+
+- <https://twitter.com/adnan_msagar>
+- <https://websemantics.ca>
+
+
+## Copyright and license
+
+Code and documentation copyright 2016 the Bootstrap Authors, Twitter, Inc. Code released under [the MIT license](https://github.com/twbs/bootstrap/blob/master/LICENSE). Docs released under [Creative Commons](https://github.com/twbs/bootstrap/blob/master/docs/LICENSE).
---
+++ docs/dist/js/themeblr.js
@@ -0,0 +1,156 @@
+/*!
+ * themeblr v1.0.0 (http://websemantics.github.io/themeblr/)
+ * Copyright 2011-2016 The Bootstrap Authors (https://github.com/websemantics/themeblr/graphs/contributors)
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+
+if (typeof jQuery === 'undefined') {
+  throw new Error('Bootstrap\'s JavaScript requires jQuery')
+}
+
++function ($) {
+  var version = $.fn.jquery.split(' ')[0].split('.')
+  if ((version[0] < 2 && version[1] < 9) || (version[0] == 1 && version[1] == 9 && version[2] < 1) || (version[0] >= 4)) {
+    throw new Error('Bootstrap\'s JavaScript requires at least jQuery v1.9.1 but less than v4.0.0')
+  }
+}(jQuery);
+
+
++function ($) {
+
+/**
+ * --------------------------------------------------------------------------
+ * Themeblr (v1.0.0): custom.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var Custom = (function ($) {
+
+    /**
+     * ------------------------------------------------------------------------
+     * Constants
+     * ------------------------------------------------------------------------
+     */
+
+    var NAME = 'custom';
+    var VERSION = '1.0.0';
+    var DATA_KEY = 'bs.custom';
+    var EVENT_KEY = '.' + DATA_KEY;
+    var DATA_API_KEY = '.data-api';
+    var JQUERY_NO_CONFLICT = $.fn[NAME];
+
+    var ClassName = {
+        ACTIVE: 'active',
+        CUSTOM: 'ctm',
+        FOCUS: 'focus'
+    };
+
+    var Selector = {
+        DATA_TOGGLE_CARROT: '[data-action^="custom"]',
+        DATA_TOGGLE: '[data-action="customss"]',
+        INPUT: 'input',
+        ACTIVE: '.active',
+        CUSTOM: '.ctm'
+    };
+
+    var Event = {
+        CLICK_DATA_API: 'click' + EVENT_KEY + DATA_API_KEY,
+        FOCUS_BLUR_DATA_API: 'focus' + EVENT_KEY + DATA_API_KEY + ' ' + ('blur' + EVENT_KEY + DATA_API_KEY)
+    };
+
+    /**
+     * ------------------------------------------------------------------------
+     * Class Definition
+     * ------------------------------------------------------------------------
+     */
+
+    var Custom = (function () {
+        function Custom(element) {
+            _classCallCheck(this, Custom);
+
+            this._element = element;
+        }
+
+        /**
+         * ------------------------------------------------------------------------
+         * Data Api implementation
+         * ------------------------------------------------------------------------
+         */
+
+        // getters
+
+        _createClass(Custom, [{
+            key: 'action',
+
+            // public
+
+            value: function action() {
+                /* called on click */
+                var triggerChangeEvent = true;
+                var rootElement = $(this._element).closest(Selector.DATA_TOGGLE)[0];
+            }
+
+            // static
+
+        }], [{
+            key: '_jQueryInterface',
+            value: function _jQueryInterface(config) {
+                return this.each(function () {
+                    var data = $(this).data(DATA_KEY);
+
+                    if (!data) {
+                        data = new Custom(this);
+                        $(this).data(DATA_KEY, data);
+                    }
+
+                    if (config === 'action') {
+                        data[config]();
+                    }
+                });
+            }
+        }, {
+            key: 'VERSION',
+            get: function get() {
+                return VERSION;
+            }
+        }]);
+
+        return Custom;
+    })();
+
+    $(document).on(Event.CLICK_DATA_API, Selector.DATA_TOGGLE_CARROT, function (event) {
+        event.preventDefault();
+
+        var custom = event.target;
+
+        if (!$(custom).hasClass(ClassName.CUSTOM)) {
+            custom = $(custom).closest(Selector.CUSTOM);
+        }
+
+        Custom._jQueryInterface.call($(custom), 'action');
+    });
+
+    /**
+     * ------------------------------------------------------------------------
+     * jQuery
+     * ------------------------------------------------------------------------
+     */
+
+    $.fn[NAME] = Custom._jQueryInterface;
+    $.fn[NAME].Constructor = Custom;
+    $.fn[NAME].noConflict = function () {
+        $.fn[NAME] = JQUERY_NO_CONFLICT;
+        return Custom._jQueryInterface;
+    };
+
+    return Custom;
+})(jQuery);
+
+}(jQuery);
